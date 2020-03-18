5d63c0867a71d3a752a8a329a110a79ad8f4870b
Akasa_Raiki <44969449+akasa885@users.noreply.github.com>
2020-03-18 23:05:29 +0700

    Initial commit

---
+++ app/src/main/java/android/latihan/tugas_praktikum_7/TittleFragment.kt
@@ -0,0 +1,18 @@
+package android.latihan.tugas_praktikum_7
+
+import android.latihan.tugas_praktikum_7.R
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import android.latihan.tugas_praktikum_7.databinding.FragmentTitleBinding
+class TitleFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title,container,false)
+        binding.playButton.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        }
+        return binding.root
+    }
+}
